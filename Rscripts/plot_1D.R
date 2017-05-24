afeda4d0aecd205988be9bc565430934044251a3
Default Seadas User <seadas-user@localhost>
2017-05-24 22:36:08 +0900

    Add plot_1D.R (for plotting time evolution of vertical patterns of variables)

---
+++ Rscripts/plot_1D.R
@@ -0,0 +1,77 @@
+library(plot3D)
+source('~/Working/FlexEFT1D/Rscripts/getData.R')
+plot_1D <- function(Var,model,Stn,title='',ZLIM=NULL, finalyr = F){
+     filedir  <- paste0('~/working/FlexEFT1D/DRAM_0.9/',model,'/',Stn,'/')
+      data    <- getData(filedir,Stn,Var)
+      days    <- data$days
+     depth    <- data$depth
+      data    <- data$data
+ 
+     if (finalyr) {
+        #Take the final year   
+      fday <- days[length(days)]  #Final day
+      cff  <- which( (days > fday-360) & (days <= fday))
+      days <- days[cff]%%360
+      days[days==0] <- 360
+      data <- data[cff,]
+    }
+    if (!is.null(ZLIM)){
+       data[data < ZLIM[1]] <- ZLIM[1]
+       data[data > ZLIM[2]] <- ZLIM[2]
+    }
+    image2D(as.matrix(data), x=days, y=-depth,zlim=ZLIM,
+           xlab='Days',ylab='Depth (m)',main=title,xaxt='n')
+    #Add Variable name and units:
+    if (Var == 'TIN'){
+      Varname  <- bquote(.(Var)~' (mmol '*m^-3*')')
+    }else if (Var == 'CHL_T'){
+      Varname  <- expression(paste("Chl "*'(mg '*m^-3*')'))
+    }else if (Var == 'NPP'){
+      Varname  <- bquote(.(Var)~' (mg C '*m^-3*' '*d^-1*')')
+    }else if (substr(Var,1,3) == 'muN'){
+      K        <- substr(Var,4,4)
+#      Varname  <- bquote('µ'~ .(K) ~' ( '*d^-1*')')
+      Varname  <- bquote('µ ( '*d^-1*')')
+    }else if (Var == 'The1'){
+      Varname  <- expression(paste("Chl:C "*' (gChl '*molC^-1*')'))
+    }else if (Var == 'QN_1'){
+      Varname  <- expression(paste("N:C "*' (molN '*molC^-1*')'))
+    }else if (Var == 'PHY1'){
+      Varname  <- expression(paste("PHY "*'(mmol '*m^-3*')'))
+    }else if (Var == 'R_PMU'){
+      Varname  <- expression(paste("Ln mean size "*'( '*µm^3*')'))
+    }else if (Var == 'R_VAR'){
+      Varname  <- expression(paste("Ln size variance "*'('*µm^3*')'^2*')'))
+    }else if (Var == 'dmudl'){
+      Varname  <- expression(paste("dµ/dL ("*d^-1*' (ln '*µm^3*')'^-1*' )'))
+    }else if (Var == 'd2mu'){
+      Varname  <- expression(paste("d2µ/dL2 ("*d^-2*' (ln '*µm^3*')'^-2*')'))
+    }else if (Var == 'd2gdl'){
+      Varname  <- expression(paste("d2g/dL2 ("*d^-2*' (ln '*µm^3*')'^-2))
+
+    }else{
+      Varname  <- bquote(.(Var) ~ ' (mmol '*m^-3*')')
+    }
+
+    mtext(Varname, side = 3, adj=1)
+
+    #Plot nutricline
+    if(Var == 'NO3') {
+      NO3cln <- numeric(nrow(data))
+      for (i in 1:nrow(data)){
+          x  <- data[i,]
+          z  <- which.min(x>1)
+      NO3cln[i] <- depth[z]
+      }
+      lines(days,-NO3cln,lwd=2,col='green')
+    }
+
+   if (!finalyr) {
+      axis(1, at=seq(0,max(days),by=360))
+      abline(  v=seq(0,max(days),by=360),lty=3)
+   }else {
+      axis(1, at=seq(15,360,by=90), labels=c('Jan','Apr','Jul','Oct'))
+   }
+}
+
+
