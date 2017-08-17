31af8658bf2b898f7add4defd409fc93d4aabcb5
Bingzhang Chen <bingzhang.chen@gmail.com>
2017-08-17 13:38:27 +0900

    Update plot_TN.R

--- Rscripts/plot_TN.R
+++ Rscripts/plot_TN.R
@@ -1,19 +1,20 @@
 source('~/Working/FlexEFT1D/Rscripts/Hz.R')
 source('~/Working/FlexEFT1D/Rscripts/getData.R')
 Hz        <- Hz(hmax=500, thetaS=2, nlev=30)
-model_ID  <- 'NPZDcont_sRun'
+model_ID  <- 'NPZDcont'
 BOTH      <- T
 Stns      <- c('S1', 'K2')
 for (Stn in Stns){
   if (BOTH){
-   filedir  <- paste0('~/working/FlexEFT1D/DRAM/',model_ID,'/BOTH/')
+   filedir  <- paste0('~/working/FlexEFT1D/DRAM/',model_ID,'/BOTH_TD/')
   } else{
    filedir  <- paste0('~/working/FlexEFT1D/DRAM/',model_ID,'/',Stn,'/')
   }
   
   setwd(filedir)
   NO3   <- getData(filedir,Stn,'NO3')
-  ZOO   <- as.matrix(getData(filedir,Stn,'ZOO')$data)
+  MIC   <- as.matrix(getData(filedir,Stn,'MIC')$data)
+  MES   <- as.matrix(getData(filedir,Stn,'MES')$data)
   DET   <- as.matrix(getData(filedir,Stn,'DET')$data)
   days  <- NO3$days
   depth <- NO3$depth
@@ -21,10 +22,11 @@ for (Stn in Stns){
   PHY   <- as.matrix(getData(filedir,Stn,'PHY_T')$data)
   #Calculate total N for each date:
   TNO3 <- NO3 %*% Hz
-  TZOO <- ZOO %*% Hz
+  TMIC <- MIC %*% Hz
+  TMES <- MES %*% Hz
   TDET <- DET %*% Hz
   TPHY <- PHY %*% Hz
-  TN   <- TNO3+TZOO+TDET+TPHY
+  TN   <- TNO3+TMIC+TDET+TPHY+TMES
   
   pdffile <- paste(Stn,model_ID,'_TN_TFe.pdf',sep='')
   
@@ -40,11 +42,12 @@ for (Stn in Stns){
     plot(days,TN,type='l',lwd=2,ylim=c(0,max(TN)))
     lines(days,TNO3,col='red')
     lines(days,TDET,col='blue')
-    lines(days,TZOO,col='green')
+    lines(days,TMIC,col='green')
+    lines(days,TMES,col='tan')
     lines(days,TPHY,col='cyan',lty=2)
-    legend('topright',legend=c('TN','NO3','DET','ZOO','PHY'),
-           col=c(1,'red','blue','green','cyan'),
-           lty=c(1,1,1,1,3))
+    legend('topright',legend=c('TN','NO3','DET','MIC','MES','PHY'),
+           col=c(1,'red','blue','green','tan','cyan'),
+           lty=c(1,1,1,1,1,2))
     
     #Calculate total Iron:
     fer   <- as.matrix(getData(filedir,Stn,'Fer'  )$data)
@@ -52,15 +55,16 @@ for (Stn in Stns){
     Fe_N  <- 0.0265
     Tfer  <- fer   %*% Hz 
     TDETfe<- DETfe %*% Hz
-    TFE   <- Tfer + TDETfe + (TZOO + TPHY)*Fe_N
+    TFE   <- Tfer + TDETfe + (TMIC + TPHY)*Fe_N
     plot(days,TFE,type='l',lwd=2,ylim=c(0,max(TFE)))
     lines(days,Tfer,  col='red')
     lines(days,TDETfe,col='blue')
-    lines(days,TZOO*Fe_N,col='green')
+    lines(days,TMIC*Fe_N,col='green')
+    lines(days,TMES*Fe_N,col='tan')
     lines(days,TPHY*Fe_N,col='cyan',lty=2)
-    legend('topright',legend=c('TFE','Tfer','TDETfe','ZOO','PHY'),
-           col=c(1,'red','blue','green','cyan'),
-           lty=c(1,1,1,1,3))
+    legend('topright',legend=c('TFE','Tfer','TDETfe','MIC','MES','PHY'),
+           col=c(1,'red','blue','green','tan','cyan'),
+           lty=c(1,1,1,1,1,2))
   
   dev.off()
 }
