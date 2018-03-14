accbd8183a624d11c94985dbf537aee3cac6e4cd
Bingzhang Chen <bingzhang.chen@gmail.com>
2018-03-15 08:20:24 +0900

    Add NPZD2sp

---
+++ DRAM/NPZD2sp/HOT/Fixed_Run.f90
@@ -0,0 +1,50 @@
+Program Single_Run
+use sub_mod
+implicit none
+real :: start, finish
+
+! The model output to match with observational data: 
+real, allocatable    :: Ymod(:)
+integer              :: i
+real                 :: mu0hat, A0N, wDET, Q0N,aI0
+
+namelist /parameters/    mu0hat, A0N, wDET, Q0N,aI0
+
+!  open the namelist file and read station name.
+open(namlst,file='param.nml',status='old',action='read')
+read(namlst,nml=parameters)
+close(namlst)
+
+call cpu_time(start) 
+singlerun = .TRUE.
+
+!Initialize the Arrays of model parameters with the biological model chosen
+call SetUpArrays
+
+! Initialize the 1D model:
+call Model_setup
+
+NDays  = 1080
+savefile = .TRUE.
+! Read model parameters:
+params(imu0)=mu0hat
+params(iwDET)=wDET
+params(iQ0N)=Q0N
+
+do i = 1, NPar
+   write(6, 101) trim(ParamLabel(i)), params(i)
+enddo
+101 format(A6, 2x, F10.3)
+
+allocate(Ymod(ANobs))
+Ymod(:) = 0d0
+call CalcYSSE(params, Ymod, SSqE)
+
+do i = 1, size(SSqE)
+   write(6, 102) 'SSqE(',i,')=', SSqE(i)
+enddo
+102 format(A5,I1,A2,1x,1pe10.3)
+call cpu_time(finish) 
+print '("Time = ",f8.3," mins.")', (finish-start)/60.0 
+
+End program
