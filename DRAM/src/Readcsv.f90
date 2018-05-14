961dfe423a9604f0213ac4c601079f895eb0e11d
Bingzhang Chen <bingzhang.chen@gmail.com>
2018-05-14 17:14:14 +0900

    Updating Readcsv.f90...

--- DRAM/src/Readcsv.f90
+++ DRAM/src/Readcsv.f90
@@ -1,15 +1,14 @@
 subroutine  Readcsv(filename,nrow,ncol,dat)
 implicit none
 character(LEN=20),intent(in)      :: filename
-integer,          intent(in)      :: nrow, ncol
+integer,          intent(out)     :: nrow, ncol
 character(LEN=10),dimension(ncol) :: header
 integer,          parameter       :: stdout=6, funit = 9
 real, dimension(nrow,ncol), intent(out):: dat
-integer    :: err, ix
+integer    :: err, ix, IOS
 logical    :: I_opened
-real       :: cff(ncol)
+character(LEN=100000) :: cff
 
-   dat(:,:) = 0.0
    ! Inquire whether the unit has been opened or not
    INQUIRE (funit, OPENED=I_opened) 
 
@@ -27,8 +26,6 @@ real       :: cff(ncol)
 
    ELSE
      read(funit,*,iostat=err) header  !read the first row
-    ! write(stdout,*)  header
-
 
      if (err .gt. 0) then 
        write(stdout,*) 'The error is: ', err, 'at Row: ',ix, ' for ',TRIM(filename)
@@ -40,25 +37,35 @@ real       :: cff(ncol)
        CLOSE(funit)
        stop
      else
-       continue
+       ! Determine nrow and ncol in the file
+
+       do while (IOS == 0)
+          read(fh, '(A)', iostat=ios) buffer
+          if (ios == 0) then
+             line = line + 1
+             
+             ! Find the first instance of whitespace.
+             ! Split label and data.
+             pos = scan(buffer, '    ')
+             label = buffer(1:pos)
+             buffer =
+                                     buffer(pos+1:)do ix = 1, nrow  
+         read(funit,*,iostat=IO) cff
+         if (err .gt. 0) then 
+           write(stdout,*) 'The error is: ', err, 'at Row: ',ix, ' for ',TRIM(filename)
+           CLOSE(funit)
+           stop
+         elseif (err .lt. 0) then
+           write(stdout,*) 'The error is: ', err, 'at Row: ',ix, ' for ',TRIM(filename)  
+           write(stdout,*) 'End of file reached!'
+           CLOSE(funit)
+           stop
+         else
+           dat(ix,:) = cff(:)
+         endif
+       enddo  
      endif
-
-     do ix = 1,nrow  
-       read(funit,*,iostat=err) cff(:)
-       if (err .gt. 0) then 
-         write(stdout,*) 'The error is: ', err, 'at Row: ',ix, ' for ',TRIM(filename)
-         CLOSE(funit)
-         stop
-       elseif (err .lt. 0) then
-         write(stdout,*) 'The error is: ', err, 'at Row: ',ix, ' for ',TRIM(filename)  
-         write(stdout,*) 'End of file reached!'
-         CLOSE(funit)
-         stop
-       else
-         dat(ix,:) = cff(:)
-       endif
-     enddo  
-
+     dat(:,:) = 0.0
    ENDIF
    CLOSE(funit)
 ! End reading data
